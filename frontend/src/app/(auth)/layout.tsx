33aa202c6760d24350f0452887874ed354e256ea
David <david.akpovi@epitech.eu>
2025-10-13 10:40:21 +0100

    chore: clean the codebase by removing the frontend pushed by error on feature of user_management

--- frontend/src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Providers } from "../provider";
-
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Providers>{children}</Providers>;
-}
