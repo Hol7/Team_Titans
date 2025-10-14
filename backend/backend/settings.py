1b7f7fc8e84aeebee95a5074187482ea83444d7d
David <david.akpovi@epitech.eu>
2025-10-14 13:52:49 +0100

    fix(backend): fixing the issue with the cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,6 +62,11 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware"
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:3000",
+]
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
